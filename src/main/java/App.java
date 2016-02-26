eb00f3c8b97f61624a9ac5cfb3b09b3f2d1716ab
Josh <silversevenfox@yahoo.com>
2016-02-26 09:57:42 -0800

    add routes and vtl to pass second integration test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,6 +28,30 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      List<Client> clients = stylist.getClients();
+      model.put("stylist", stylist);
+      model.put("clients", clients);
+      model.put("template", "templates/addClient.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.queryParams("stylistId")));
+      int id = Integer.parseInt(request.queryParams("stylistId"));
+      String clientName = request.queryParams("clientName");
+      Client newClient = new Client(clientName, id);
+      newClient.save();
+      List<Client> clients = stylist.getClients();
+      model.put("stylist", stylist);
+      model.put("clients", clients);
+      model.put("template", "templates/addClient.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 
   }
 }
