dd6230b177acfbda857b8c118ef704cca41ed059
Josh <silversevenfox@yahoo.com>
2016-02-26 09:34:27 -0800

    add functionality to pass fourth StylistTest for find()

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -44,4 +44,14 @@ public class Stylist {
     }
   }
 
+  public static Stylist find(int id) {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "SELECT * FROM Stylists where id=:id";
+    Stylist Stylist = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Stylist.class);
+    return Stylist;
+  }
+}
+
 }
