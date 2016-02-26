b446e77a510d2fbcb81059a741da0ead699cd200
Josh <silversevenfox@yahoo.com>
2016-02-26 08:39:56 -0800

    add functionality to pass first clientTest for all()

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,5 +2,19 @@ import java.util.List;
 import org.sql2o.*;
 
 public class Client {
+  private int id;
+  private int stylistId;
+  private String name;
 
+  public Client(String name, int stylistId) {
+    this.name = name;
+    this.stylistId = stylistId;
+  }
+
+  public static List<Client> all() {
+    String sql = "SELECT id, name, stylistId FROM clients";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Client.class);
+    }
+  }
 }
