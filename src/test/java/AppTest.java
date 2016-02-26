19686ac480b2b93a54a564cf66291b4653fcecde
Josh <silversevenfox@yahoo.com>
2016-02-26 09:46:41 -0800

    add first integration test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,29 +1,35 @@
-// import org.junit.*;
-// import static org.junit.Assert.*;
-// import static org.fluentlenium.core.filter.FilterConstructor.*;
-//
-// import java.util.ArrayList;
-//
-// import org.fluentlenium.adapter.FluentTest;
-// import org.junit.ClassRule;
-// import org.junit.Test;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-//
-// import static org.assertj.core.api.Assertions.assertThat;
-//
-// public class AppTest extends FluentTest {
-//   public WebDriver webDriver = new HtmlUnitDriver();
-//   public WebDriver getDefaultDriver() {
-//       return webDriver;
-//   }
-//
-//   @Rule
-//   public DatabaseRule database = new DatabaseRule();
-//
-//
-//   @ClassRule
-//   public static ServerRule server = new ServerRule();
-//
-//
-// }
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
+import java.util.ArrayList;
+
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void stylistIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    fill("#stylistName").with("Stylist 1");
+    submit(".btn-primary");
+    assertThat(pageSource()).contains("Stylist 1");
+  }
+}
