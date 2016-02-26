28eb0f4c4f15d9b8f461b1636b7442589f4d1dee
Josh <silversevenfox@yahoo.com>
2016-02-26 09:26:09 -0800

    add functionality to pass first StylistTest for all()

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -2,5 +2,26 @@ import java.util.List;
 import org.sql2o.*;
 
 public class Stylist {
+  private int id;
+  private String name;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Stylist(String name) {
+    this.name = name;
+  }
+
+  public static List<Stylist> all() {
+    String sql = "SELECT id, name FROM Stylists";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Stylist.class);
+    }
+  }
 
 }
